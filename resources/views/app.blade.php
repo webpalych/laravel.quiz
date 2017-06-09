8a250a31e128fc7e89ab10a475d32b7efdf2d099
webpalych <palpalychbrykov@yandex.ru>
2017-06-09 11:24:54 +0300

    app view

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Quiz</title>
+    <!--   <base href="/"> -->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<my-app>Loading AppComponent content here ...</my-app>
+<script type="text/javascript" src="application/inline.bundle.js"></script>
+<script type="text/javascript" src="application/polyfills.bundle.js"></script>
+<script type="text/javascript" src="application/styles.bundle.js"></script>
+<script type="text/javascript" src="application/vendor.bundle.js"></script>
+<script type="text/javascript" src="application/main.bundle.js"></script>
+</body>
+</html>
