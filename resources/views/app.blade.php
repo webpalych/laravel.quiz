a74fb5a171a47d0ce05379a3895cb99b0e0652e1
webplaych <palpalychbrykov@yandex.ru>
2017-06-12 13:59:45 +0200

    view change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,14 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Quiz</title>
-    <!--   <base href="/"> -->
+  <meta charset="utf-8">
+  <title>Quiz</title>
+  <!-- <base href="/"> -->
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
-    <link rel="stylesheet" href="styles.css">
-</head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link href="application/styles.2ba9094fbadbc0b46cc4.bundle.css" rel="stylesheet"/></head>
 <body>
-<my-app>Loading AppComponent content here ...</my-app>
-<script type="text/javascript" src="application/inline.bundle.js"></script>
-<script type="text/javascript" src="application/polyfills.bundle.js"></script>
-<script type="text/javascript" src="application/styles.bundle.js"></script>
-<script type="text/javascript" src="application/vendor.bundle.js"></script>
-<script type="text/javascript" src="application/main.bundle.js"></script>
-</body>
+  <my-app>Loading AppComponent content here ...</my-app>
+<script type="text/javascript" src="application/inline.5b5c3f8b4826a2fadd3d.bundle.js"></script><script type="text/javascript" src="application/polyfills.1603125c517d13cb6b18.bundle.js"></script><script type="text/javascript" src="application/vendor.3461f0ab9cac93552fa4.bundle.js"></script><script type="text/javascript" src="application/main.f0f4c9452bd85e4ec6ba.bundle.js"></script></body>
 </html>
